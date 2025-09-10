4c0266bea9c342c835a407b4eac90c59d63639fa
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2025-09-09 23:31:26 -0300

    feat: integrate Sanity CMS with startup and author schemas, implement data fetching

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,13 @@
-import StartupCard from '@/components/StartupCard'
+import { client } from '@/sanity/lib/client';
+import { STARTUPS_QUERY } from '@/sanity/lib/queries';
+
+import StartupCard, { StartupCardType } from '@/components/StartupCard';
 import SearchForm from '../../components/SearchForm';
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
   const query = (await searchParams).query;
 
-  const posts = [
-    {
-      _createdAt: new Date(),
-      views: 123,
-      author: { _id: 1, name: 'Luke Smith' },
-      _id: 1,
-      description: 'This is a sample description',
-      image:
-        'https://cdn1.epicgames.com/offer/428115def4ca4deea9d69c99c5a5a99e/EN_Bungie_D2_S27_OfferLandscape_S1_2560x1440_2560x1440-0aa20ea10d57acb82c0fee4699a447cc',
-      category: 'Games',
-      title: 'Destiny 2',
-    },
-  ];
+  const posts = await client.fetch(STARTUPS_QUERY);
 
   return (
     <>
@@ -31,15 +22,13 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
       </section>
 
       <section className="section_container">
-        <p className='text-30-semibold'>{query ? `Search Results for "${query}"` : 'All Startups'}</p>
+        <p className="text-30-semibold">{query ? `Search Results for "${query}"` : 'All Startups'}</p>
 
-        <ul className='mt-7 card_grid'>
+        <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupTypeCard, index: number) => (
-              <StartupCard key={post._id} post={post} />
-            ))
+            posts.map((post: StartupCardType, index: number) => <StartupCard key={post._id} post={post} />)
           ) : (
-            <p className='no-results'>No startups found</p>
+            <p className="no-results">No startups found</p>
           )}
         </ul>
       </section>
