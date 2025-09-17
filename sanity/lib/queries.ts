ccfbb9bf222caee985f9c71ab96c483e62852688
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2025-09-16 23:16:13 -0300

    feat: create user profile page and add author query for fetching user details

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -36,7 +36,7 @@ export const STARTUP_VIEWS_QUERY = defineQuery(`
   *[_type == "startup" && _id == $id][0]{
     _id, views
   }
-  `);
+`);
 
 export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`
   *[_type == "author" && id == $id][0]{
@@ -48,4 +48,16 @@ export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`
     image,
     bio
   }
-  `);
+`);
+
+export const AUTHOR_BY_ID_QUERY = defineQuery(`
+  *[_type == "author" && _id == $id][0]{
+    _id,
+    id,
+    name,
+    username,
+    email,
+    image,
+    bio
+  }
+`);
